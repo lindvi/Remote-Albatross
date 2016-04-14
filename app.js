0e8cad65663a2e6e7d8f11c641de37a9260594b2
Charlie Lindviken <charlie.lindviken@bontouch.com>
2016-04-14 19:30:03 +0200

    Woop woop

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
+var EMPTYQUEUE = process.argv[2] === '--clean' ? true : false;
 
 var request = require('request');
+var kue = require('kue'),
+queue = kue.createQueue();
+kue.app.listen(8080);
 
 var twitterData = [];
 
@@ -13,22 +17,42 @@ var states = {
 	failed: 0
 };
 
-request('http://localhost:3000/tweet_imports.json', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-   	console.log(body);
-    twitterData = body;
-    states.twitterdata = true;
-    startKue();
-  } else {
-  	console.log(error);
-  	console.log(response);
-  }
-});
-
+if(!EMPTYQUEUE) {
+	request('http://localhost:3000/tweet_imports.json', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			twitterData = body;
+			states.twitterdata = true;
+			startKue();
+		} else {
+			console.log(error);
+			console.log(response);
+		}
+	});
+}
 
 function startKue() {
-	
+	_.each(twitterData, function(tweet){
+		var job = queue.create('twitter_request', {
+			tweet: tweet
+		}).removeOnComplete(true).save();
+	});
 }
+
+
+queue.process('twitter_request', 1, function(job, done){
+	//Call with id
+
+	/*
+	request('http://localhost:3000/twitter_connection/', function (error, response, body) {
+			if(!error){
+
+			}
+		});
+		*/
+	console.log(job.data.tweet.id);
+	done && done();
+});
 
 var outputDownload = function status() {
 	console.info('########');
