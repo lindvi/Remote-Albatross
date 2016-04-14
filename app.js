e3c04eec109a56fd5174dff683b5ad25cb314afa
Charlie Lindviken <charlie.lindviken@bontouch.com>
2016-04-14 19:15:00 +0200

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var request = require('request');
+
+
+
+var outputDownload = function status() {
+	console.info('########');
+	console.info(new Date());
+}
+
+
+setInterval(outputDownload, 1000);
+
+
+
