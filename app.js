dc238bb8c4978d966c68e762de3e0866cc30becd
Charlie Lindviken <charlie.lindviken@bontouch.com>
2016-04-14 19:19:29 +0200

    wat

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
 var request = require('request');
 
+var twitterData = [];
 
+var status = {
+	twitterdata: false
+};
+
+request('http://www.localhost:3000/twitter_imports.json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    twitterData = body;
+    status.twitterdata = true;
+  }
+});
 
 var outputDownload = function status() {
 	console.info('########');
 	console.info(new Date());
+	console.info('TwitterData loaded: ', status.twitterdata);
 }
 
 
